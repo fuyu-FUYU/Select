4f2b0532b82c25927e4a85f52965f6c1f293f1e8
fuyu <fuyu@123.com>
2020-05-31 21:31:39 +0800

    add

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- 向Input进行传值 -->
+    <Input v-bind:subject="subject" />
+  </div>
+</template>
+
+<script>
+import Input from "@/views/aa.vue";
+export default {
+  name: "App",
+  components: {
+    Input
+  },
+  data() {
+    return {
+      subject: [
+        {
+          value: "1",
+          text: "111"
+        },
+         {
+          value: "2",
+          text: "222"
+        },
+        {
+          value: "3",
+          text: "333"
+        },
+        {
+          value: "4",
+          text: "444"
+        }
+      ]
+    };
+  }
+};
+</script>
